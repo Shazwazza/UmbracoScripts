41b1405900083fb87074c2f228dfff8047b1b71c
Shannon Deminick <sdeminick@gmail.com>
2017-06-29 16:18:00 +1000

    CHanges to c#

--- src/LinqPad/RestoreContentFromCacheFile.linq.cs
+++ src/LinqPad/RestoreContentFromCacheFile.linq.cs
@@ -1,11 +1,3 @@
-<Query Kind="Statements">
-  <Connection>
-    <ID>96c5d150-f440-4496-b12e-479e61869702</ID>
-    <Persist>true</Persist>
-    <Driver Assembly="UmbracoLinqPad" PublicKeyToken="977d3d694b4b0d0b">UmbracoLinqPad.UmbracoDynamicDriver</Driver>
-    <AppConfigPath>X:\Projects\~WebSitesTesting\CourierTest1\CourierTest1</AppConfigPath>
-  </Connection>
-</Query>
 
 var contentXmlFile = new FileInfo("X:\\TEMP\\umbraco.config");
 if (!contentXmlFile.Exists)
