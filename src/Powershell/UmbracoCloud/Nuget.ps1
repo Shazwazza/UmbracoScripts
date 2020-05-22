f2d3412ca12db9cb1105cde61bffaf47ddced5ef
Shannon <sdeminick@gmail.com>
2020-05-22 18:17:51 +1000

    Gets the whole thing working including install.ps1 files, but cant' figure out the deletd files

--- src/Powershell/UmbracoCloud/Nuget.ps1
+++ src/Powershell/UmbracoCloud/Nuget.ps1
@@ -1,5 +1,4 @@
-function Get-NugetExe
-{
+function Get-NugetExe {
     [CmdletBinding(DefaultParameterSetName = 'None')]
     param(
         [Parameter(Mandatory)]
@@ -11,20 +10,21 @@ function Get-NugetExe
     $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
     $nugetExePath = Join-Path $DestinationFolder "nuget.exe"
 
-    if (-not (Test-Path $nugetExePath -PathType Leaf)) 
-    {
+    if (-not (Test-Path $nugetExePath -PathType Leaf)) {
         Invoke-WebRequest $sourceNugetExe -OutFile $nugetExePath
     }
 
     return $nugetExePath
 }
 
-function Update-NugetPackage
-{
+function Update-NugetPackage {
     [CmdletBinding(DefaultParameterSetName = 'None')]
     param(
         [Parameter(Mandatory)]
         [string] $PackageName,
+
+        [Parameter(Mandatory)]
+        [string] $PackageVersion,
 
         [Parameter(Mandatory)]
         [string] $ProjectFile,
@@ -42,7 +42,7 @@ function Update-NugetPackage
     )
 
     $projFile = Get-Item $ProjectFile
-    if ($projFile.Exists -eq $false){
+    if ($projFile.Exists -eq $false) {
         throw "The project file does not exist $ProjectFile"
     }
 
@@ -59,15 +59,13 @@ function Update-NugetPackage
 
     # First we need to do a nuget restore
     $nugetResult = Invoke-NugetRestore -NugetExe "$NugetExe" -ProjectFile "$ProjectFile" -NugetConfigFile "$nugetConfigFilePath" -PackagesPath "$packagesPath" -MSBuildPath "$MSBuildPath"
-    if ($nugetResult -eq $true)
-    {
+    if ($nugetResult -eq $true) {
         # Then we can do a nuget update
-        Invoke-NugetUpdate -NugetExe "$NugetExe" -PackageName "$PackageName" -ProjectFile "$ProjectFile" -NugetConfigFile "$nugetConfigFilePath" -PackagesPath "$packagesPath" -MSBuildPath "$MSBuildPath" -Safe:$Safe
+        Invoke-NugetUpdate -NugetExe "$NugetExe" -PackageName "$PackageName" -PackageVersion "$PackageVersion" -ProjectFile "$ProjectFile" -NugetConfigFile "$nugetConfigFilePath" -PackagesPath "$packagesPath" -MSBuildPath "$MSBuildPath" -Safe:$Safe
     }
 }
 
-function Invoke-NugetRestore
-{
+function Invoke-NugetRestore {
     [CmdletBinding(DefaultParameterSetName = 'None')]
     param(
         [Parameter(Mandatory)]
@@ -94,7 +92,7 @@ function Invoke-NugetRestore
 
     & $NugetExe restore "$ProjectFile" -ConfigFile "$NugetConfigFile" -PackagesDirectory "$PackagesPath" -Project2ProjectTimeOut 20 -NonInteractive -MSBuildPath "$MSBuildPath"
 
-    if($LASTEXITCODE -eq 0) {
+    if ($LASTEXITCODE -eq 0) {
         return $true
     }
     else {
@@ -102,8 +100,7 @@ function Invoke-NugetRestore
     }
 }
 
-function Invoke-NugetUpdate
-{
+function Invoke-NugetUpdate {
     [CmdletBinding(DefaultParameterSetName = 'None')]
     param(
         [Parameter(Mandatory)]
@@ -111,6 +108,9 @@ function Invoke-NugetUpdate
 
         [Parameter(Mandatory)]
         [string] $PackageName,
+
+        [Parameter(Mandatory)]
+        [string] $PackageVersion,
 
         [Parameter(Mandatory)]
         [string] $ProjectFile,
@@ -131,29 +131,159 @@ function Invoke-NugetUpdate
         throw "The Nuget exe file does not exist $NugetExe"
     }
 
+    # Get all csproj files that we are upgrading
+    $csProjs = Get-CSProjFilesForUpdate -PackageName $PackageName -PackageVersion $PackageVersion -ProjectFile $ProjectFile
+
     Write-Verbose "Running Nuget update..."
 
-    # TODO: 'overwrite' is not right, but IgnoreAll and Ignore ends up deleting files!@
-    # TODO: Try running the update/build against the SLN instead of just the csproj!
-
     if ($Safe) {
-        & $NugetExe update "$ProjectFile" -ConfigFile "$NugetConfigFile" -RepositoryPath "$PackagesPath" -Id $PackageName -FileConflictAction overwrite -NonInteractive -MSBuildPath "$MSBuildPath" -safe
+        & $NugetExe update "$ProjectFile" -ConfigFile "$NugetConfigFile" -RepositoryPath "$PackagesPath" -Id $PackageName -Version $PackageVersion -FileConflictAction IgnoreAll -NonInteractive -MSBuildPath "$MSBuildPath" -safe -Verbosity detailed
     }
     else {
-        & $NugetExe update "$ProjectFile" -ConfigFile "$NugetConfigFile" -RepositoryPath "$PackagesPath" -Id $PackageName -FileConflictAction overwrite -NonInteractive -MSBuildPath "$MSBuildPath"
+        & $NugetExe update "$ProjectFile" -ConfigFile "$NugetConfigFile" -RepositoryPath "$PackagesPath" -Id $PackageName -Version $PackageVersion -FileConflictAction IgnoreAll -NonInteractive -MSBuildPath "$MSBuildPath" -Verbosity detailed
     }
-    
-    if($LASTEXITCODE -eq 0) {
-        return $true
-    }
-    else {
+
+    if (!($LASTEXITCODE -eq 0)) {
         throw "An error occurred, quitting"
+    }
+
+    # Run the install.ps1 scripts in the nuget tools for each project updated
+    foreach($csProj in $csProjs) {
+        Write-Verbose "Finding tools/install.ps1 for $csProj"
+        Invoke-NugetToolsScripts -PackagesPath $PackagesPath -PackageName $PackageName -PackageVersion $PackageVersion -CsProjFile $csProj
+    }    
+}
+
+function Invoke-NugetToolsScripts {
+    [CmdletBinding(DefaultParameterSetName = 'None')]
+    param(
+        [Parameter(Mandatory)]
+        [string] $PackagesPath,
+
+        [Parameter(Mandatory)]
+        [string] $PackageName,
+
+        [Parameter(Mandatory)]
+        [string] $PackageVersion,
+
+        [Parameter(Mandatory)]
+        [string] $CsProjFile
+    )
+
+    # TODO: After running this we need to manually call the /tools/install.ps1 script - since we don't know what they will do we can filter this to only run
+    # when it's the UmbracoCms. But to do that we need to pass in the correct params which is difficult to figure out. 
+    # the script takes these params param($installPath, $toolsPath, $package, $project)
+    # * installPath: $installPath is the path to the folder where the package is installed. By default: $(solutionDir)\packages
+    #       ... which (for example): C:\Users\Shannon\Documents\_Projects\Shazwazza\Repo\shazwazza.com\src\packages\UmbracoCms.8.6.1
+    # * $toolPath is the path to the \tools directory in the folder where the package is installed. By default: $(solutionDir)\packages\[packageId]-[version]\tools
+    #       ... which (for example): C:\Users\Shannon\Documents\_Projects\Shazwazza\Repo\shazwazza.com\src\packages\UmbracoCms.8.6.1\tools
+    # * $package is a reference to the package object 
+    #       ... NOT USED FOR UMBRACO
+    # * $project is a reference to the target EnvDTE project object. This object is defined here.
+    #       ... $projectPath = (Get-Item $project.Properties.Item("FullPath").Value).FullName is used for UMBRACO
+    #       ... which (for example): C:\Users\Shannon\Documents\_Projects\Shazwazza\Repo\shazwazza.com\src\Shazwazza.Web\
+    # see https://stackoverflow.com/a/41999946/694494
+    # The source file for the VS console nuget manager is here 
+    # C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE\CommonExtensions\Microsoft\NuGet\Modules\NuGet
+
+    # first we need to construct the "installPath"
+    $installPath = Join-Path $PackagesPath "$PackageName.$PackageVersion"
+    if (!(Test-Path -Path $installPath)) {
+        throw "Could not find the installed package folder at $installPath"
+    }
+    $toolsPath = Join-Path $installPath "tools"
+    if (Test-Path -Path $toolsPath) {
+        $installScript = Join-Path $toolsPath "install.ps1"
+        if (Test-Path -Path $installScript) {
+            # Ok, we can execute
+
+            # The last step is to 'fake' the $project object
+            $projectProperties = New-Object -TypeName PSObject
+            $projectProperties | Add-Member -MemberType ScriptMethod -Name Item -Value {
+                param([string]$val)
+                return @{
+                    # return the path of the project being updated
+                    Value = (Get-Item $CsProjFile).Directory.FullName
+                }
+            }
+            $project = New-Object -TypeName PSObject
+            $project | Add-Member -MemberType NoteProperty -Name Properties -Value $projectProperties
+
+            # Then we need to fake a $DTE object
+            $itemOps = New-Object -TypeName PSObject
+            $itemOps | Add-Member -MemberType ScriptMethod -Name OpenFile -Value {
+                param([string]$val)
+                # nop
+            }
+            $DTE = New-Object -TypeName PSObject
+            $DTE | Add-Member -MemberType NoteProperty -Name ItemOperations -Value $itemOps
+
+            Write-Verbose "Found tools/install.ps1 script in Nuget package. Executing script..."
+            & $installScript $installPath $toolsPath $null $project | Out-Null
+            Write-Verbose "tools/install.ps1 completed"
+        }
     }
 }
 
-function Find-NugetConfig
-{
-   [CmdletBinding(DefaultParameterSetName = 'None')]
+function Get-CSProjFilesForUpdate {
+
+    [CmdletBinding(DefaultParameterSetName = 'None')]
+    param(
+        [Parameter(Mandatory)]
+        [string] $PackageName,
+
+        [Parameter(Mandatory)]
+        [string] $PackageVersion,
+
+        [Parameter(Mandatory)]
+        [string] $ProjectFile
+    )
+
+    Add-Type -AssemblyName System.Xml.Linq
+
+    # the project file can be an sln or a csproj. If it's an sln, get all csproj's for it
+    # then check if they will be upgraded
+
+    # Get all csproj's that were upgraded
+    $csprojs = New-Object Collections.Generic.List[string]
+    $fileExt = [System.IO.Path]::GetExtension($ProjectFile)
+    if ($fileExt -eq ".sln") {
+        $slnPath = (Get-Item $ProjectFile).Directory.FullName
+        # read in sln file contents
+        $slnContent = Get-Content -Path $ProjectFile -Raw
+        # parse out all csproj's
+        $regex = "^Project\(\`".+?, \`"(.+\.csproj)\`""
+        $m = [regex]::Matches($slnContent, $regex, [System.Text.RegularExpressions.RegexOptions]::Multiline)
+        # add all csproj's to the list
+        foreach ($i in $m) {
+            $csprojRelativePath = $i.Groups[1].Value;
+            $csprojAbsPath = Join-Path -Path $slnPath -ChildPath $csprojRelativePath
+            # get the packages.config file for this csproj to see if it references the package            
+            $packagesConfig = (Get-Item $csprojAbsPath).Directory.GetFiles("packages.config")
+            if ($packagesConfig.Length -gt 0){
+                
+                # if the version for this csproj doesn't match our target version then add it to the list
+                $xml = [System.Xml.Linq.XDocument]::Load($packagesConfig[0].FullName)
+                $xpath = "string(//package[@id='$PackageName']/@version)"
+                $version = [string][System.Xml.XPath.Extensions]::XPathEvaluate($xml, $xpath);    
+                
+                if ($PackageVersion -ne $version){
+                    $csprojs.Add($csprojAbsPath)
+                }
+            }            
+        }
+    }
+    else {
+        $csprojs.Add($ProjectFile)
+    }
+
+    Write-Verbose "The .csproj files being updated are $csprojs"
+
+    return $csprojs
+}
+
+function Find-NugetConfig {
+    [CmdletBinding(DefaultParameterSetName = 'None')]
     param(
         [Parameter(Mandatory)]
         [string] $CurrentDirectory,
@@ -168,15 +298,14 @@ function Find-NugetConfig
 
     $nugetConfigFiles = Get-ChildItem -Path $CurrentDirectory -Filter "NuGet.config"
 
-    if ($nugetConfigFiles.Count -eq 0)
-    {
+    if ($nugetConfigFiles.Count -eq 0) {
         if ($CurrentDirectory.ToLower() -eq $RootGitDirectory.ToLower()) {
             throw "No Nuget.config file found in repository"
         }   
 
         # move up
         $parent = $folder.Parent;
-        if ($null -eq $parent -or $parent.Exists -eq $false){
+        if ($null -eq $parent -or $parent.Exists -eq $false) {
             throw "No Nuget.config file found on file system"
         }   
 
@@ -188,8 +317,7 @@ function Find-NugetConfig
     return $nugetConfigFiles[0];
 }
 
-function Get-LatestPackageVersion
-{
+function Get-LatestPackageVersion {
     <#
     .SYNOPSIS
         Gets the latest version of a package from a Nuget package repository
@@ -209,15 +337,14 @@ function Get-LatestPackageVersion
     $nugetVersions = $nugetOutput.Split([System.Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
 
     $latestVersion = $nugetVersions |
-        Sort-Object { ([semver] $_.Split(' ')[1]) } |
-            Select-Object -Last 1
+    Sort-Object { ([semver] $_.Split(' ')[1]) } |
+    Select-Object -Last 1
     $latestSemver = $latestVersion.Split(' ')[1]
 
     return $latestSemver
 }
 
-function Get-UpgradeAvailable
-{
+function Get-UpgradeAvailable {
     <#
     .SYNOPSIS
         Compares to semver versions and returns $true if the DestVersion is greater than SourceVersion
--- src/Powershell/UmbracoCloud/UmbracoCloud.ps1
+++ src/Powershell/UmbracoCloud/UmbracoCloud.ps1
@@ -77,12 +77,22 @@ function Invoke-PackageAutoUpgrade {
         Checks for package updates and if there are any will upgrade the source code repository and submit a Pull Requet.
     .PARAMETER PackageName
         The Nuget package name to check for upgrades.
+    .PARAMETER ProjectFile
+        The full path to the sln or csproj file to upgrade.
+    .PARAMETER PackageFile
+        The virtual path to the packages.config file to check for ugprades on the GitHub repository.
     .PARAMETER GitHubOwner
         The GitHub owner name.
     .PARAMETER GitHubRepository
         The GitHub repository name.
     .PARAMETER GitHubAccessToken
         The GitHub access token.
+    .PARAMETER DoNotCommit
+        Flag if set will not commit changes and therefor not push or create a PR
+    .PARAMETER DoNotPush
+        Flag if set will not push committed changes and therefor not create a PR
+    .PARAMETER DoNotPR
+        Flag if set will not create a PR from the pushed and committed changes
     #>
     [CmdletBinding(DefaultParameterSetName = 'None')]
     param
@@ -92,6 +102,12 @@ function Invoke-PackageAutoUpgrade {
     
         [String] [Parameter(Mandatory = $true)]
         $ProjectFile,
+
+        # TODO: don't explicitly pass this in, if its an sln file we need to loop over the csproj files and automatically detect, 
+        # same with a csproj we should auto-detect this path, but that might be difficult to do since we don't know how the repo is structured.
+        # For now, this is fine.
+        [String] [Parameter(Mandatory = $true)]
+        $PackageFile,
     
         [String] [Parameter(Mandatory = $true)]
         $GitHubOwner,
@@ -100,85 +116,102 @@ function Invoke-PackageAutoUpgrade {
         $GitHubRepository,
     
         [String] [Parameter(Mandatory = $true)]
-        $GitHubAccessToken
+        $GitHubAccessToken,
+
+        [switch]
+        $DoNotCommit,
+        [switch]
+        $DoNotPush,
+        [switch]
+        $DoNotPR
     )
     
     # Variables
     
     $buildFolder = $PSScriptRoot
-    $tempFolder = "$buildFolder\_TEMP"
+    $tempFolder = "$env:temp\UmbracoCloudPs"
     $repoRoot = (Get-Item $buildFolder).Parent
     
-    # Get version in GitHub
-    # TODO: Cannot hard code file
-    Write-Verbose "Getting latest version of $PackageName from GitHub"
-    $packageVersion = Get-CurrentPackageVersion -OwnerName $GitHubOwner -RepositoryName $GitHubRepository -AccessToken $GitHubAccessToken -PackageFile "src/Shazwazza.Web/packages.config" -PackageName $PackageName
-    if (!$packageVersion) {
-        Throw "Could not determine package version, cannot continue"
-    }
-    Write-Verbose "Latest local version of $PackageName is $packageVersion"
-    
-    # Get the latest version from Nuget
-    
-    Write-Verbose "Getting latest version of $PackageName from Nuget"
-    $nuget = Get-NugetExe -DestinationFolder $tempFolder
-    $latest = Get-LatestPackageVersion -PackageName $PackageName
-    if (!$packageVersion) {
-        Throw "Could not determine package version, cannot continue"
-    }
-    Write-Verbose "Latest nuget version of $PackageName is $latest"
-    
-    # Compare versions, next we need to run nuget + PR
-    $hasUpgrade = Get-UpgradeAvailable -SourceVersion $packageVersion -DestVersion $latest
-    
-    if ($hasUpgrade -eq $true) {
-        Write-Verbose "An upgrade is available!"
-    
-        $branchName = "$PackageName-upgrade-$latest";
-    
-        Write-Verbose "Checking if a PR is already created..."
-        $pr = Get-PullRequest -OwnerName $GitHubOwner -RepositoryName $GitHubRepository -AccessToken $GitHubAccessToken -BranchName $branchName
-        if ($pr) {
-            throw "A Pull Request already exists for this upgrade"
+    try {
+        # Get version in GitHub
+        Write-Verbose "Getting latest version of $PackageName from GitHub"
+        $packageVersion = Get-CurrentPackageVersion -OwnerName $GitHubOwner -RepositoryName $GitHubRepository -AccessToken $GitHubAccessToken -PackageFile $PackageFile -PackageName $PackageName
+        if (!$packageVersion) {
+            Throw "Could not determine package version, cannot continue"
         }
+        Write-Verbose "Latest local version of $PackageName is $packageVersion"
     
-        $msbuild = Get-MSBuildExe -DestinationFolder $tempFolder -NugetExe $nuget
-        Write-Verbose "MSBuild found at $msbuild"
-        $msbuildPath = (Get-Item $msbuild).Directory.FullName
+        # Get the latest version from Nuget
     
-        Write-Verbose "Creating Git Branch '$branchName' ..."
-        $branchExists = Get-GitBranchExists -BranchName $branchName
-        if ($branchExists -eq $true) {
-            Write-Verbose "Branch $branchName already exists, updating to branch"
-            Switch-GitBranch -BranchName $branchName
-            # throw "Branch $branchName already exists"
+        Write-Verbose "Getting latest version of $PackageName from Nuget"
+        $nuget = Get-NugetExe -DestinationFolder $tempFolder
+        $latest = Get-LatestPackageVersion -PackageName $PackageName
+        if (!$packageVersion) {
+            Throw "Could not determine package version, cannot continue"
+        }
+        Write-Verbose "Latest nuget version of $PackageName is $latest"
+    
+        # Compare versions, next we need to run nuget + PR
+        $hasUpgrade = Get-UpgradeAvailable -SourceVersion $packageVersion -DestVersion $latest
+    
+        if ($hasUpgrade -eq $true) {
+            Write-Verbose "An upgrade is available!"
+    
+            $branchName = "$PackageName-upgrade-$latest";
+    
+            Write-Verbose "Checking if a PR is already created..."
+            $pr = Get-PullRequest -OwnerName $GitHubOwner -RepositoryName $GitHubRepository -AccessToken $GitHubAccessToken -BranchName $branchName
+            if ($pr) {
+                throw "A Pull Request already exists for this upgrade"
+            }
+    
+            $msbuild = Get-MSBuildExe -DestinationFolder $tempFolder -NugetExe $nuget
+            Write-Verbose "MSBuild found at $msbuild"
+            $msbuildPath = (Get-Item $msbuild).Directory.FullName
+    
+            Write-Verbose "Creating Git Branch '$branchName' ..."
+            $branchExists = Get-GitBranchExists -BranchName $branchName
+            if ($branchExists -eq $true) {
+                Write-Verbose "Branch $branchName already exists, updating to branch"
+                Switch-GitBranch -BranchName $branchName
+                # throw "Branch $branchName already exists"
+            }
+            else {
+                New-GitBranch -BranchName $branchName
+            }
+        
+            Write-Verbose "Upgrading project..."
+            Update-NugetPackage -PackageName $PackageName -PackageVersion $latest -ProjectFile $ProjectFile -RootGitDirectory $($repoRoot.FullName) -NugetExe $nuget -MSBuildPath $msbuildPath
+    
+            # Write-Verbose "Building project..."
+            # Build-Project -MSBuildExe $msbuild -ProjectFile $ProjectFile
+    
+            # TODO: Potentially we need to revert all /Config/* files because this will overright them and we dont want to commit those changes
+            # However in some cases we might want to see what those changes are so for now we'll leave it up to the developer to review the changes
+            # and revert what they want.
+            # This all depends on the Nuget behavior IgnoreAll, etc...
+
+            if ($false -eq $DoNotCommit) {
+                Write-Verbose "Committing changes..."
+                Add-GitChanges -Message "Updated files for the $PackageName $latest Nuget upgrade"
+            
+                if ($false -eq $DoNotPush) {
+                    Write-Verbose "Pushing changes..."
+                    Push-GitChanges -BranchName $branchName
+            
+                    if ($false -eq $DoNotPR) {
+                        Write-Verbose "Creating pull request..."                
+                        $pr = New-PullRequest -OwnerName $GitHubOwner -RepositoryName $GitHubRepository -AccessToken $GitHubAccessToken -PackageVersion $latest -PackageName $PackageName -BranchName $branchName
+                    }
+                }
+            }
         }
         else {
-            New-GitBranch -BranchName $branchName
+            Write-Verbose "Nothing to upgrade"
         }
-        
-        Write-Verbose "Upgrading project..."
-        Update-NugetPackage -PackageName $PackageName -ProjectFile $ProjectFile -RootGitDirectory $($repoRoot.FullName) -NugetExe $nuget -MSBuildPath $msbuildPath    
-    
-        Write-Verbose "Building project..."
-        Build-Project -MSBuildExe $msbuild -ProjectFile $ProjectFile
-    
-        # TODO: Potentially we need to revert all /Config/* files because this will overright them and we dont want to commit those changes
-        # However in some cases we might want to see what those changes are so for now we'll leave it up to the developer to review the changes
-        # and revert what they want.
-    
-        Write-Verbose "Committing changes..."
-        Add-GitChanges -Message "Updated files for the $PackageName $latest Nuget upgrade"
-        
-        Write-Verbose "Pushing changes..."
-        Push-GitChanges -BranchName $branchName
-        
-        Write-Verbose "Creating pull request..."
-        $pr = New-PullRequest -OwnerName $GitHubOwner -RepositoryName $GitHubRepository -AccessToken $GitHubAccessToken -PackageVersion $latest -PackageName $PackageName -BranchName $branchName
     }
-    else {
-        Write-Verbose "Nothing to upgrade"
+    finally {
+        # Clear temp files
+        Remove-Item $tempFolder -Recurse
     }
-    
-
 }
